6ae6bde9bcff7d645d58ad305f3be9a16e44efa2
aditya <adityamahaputra18@gmail.co>
2016-09-09 01:21:08 +0700

    Update From Aditya Ganteng

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+    $(function() {
+        $(".element").typed({
+            strings: ["Cepat", "Profesional", "Inovatif", "Kreatif ^2000 "],
+            typeSpeed: 10,
+            contentType: 'html',
+            loop: true,
+        });
+    });
+    $(function() {
+        $(".beranda").typed({
+            strings: ["BERANDA"],
+            typeSpeed: 0,
+            contentType: 'html',
+            showCursor: false,
+        });
+    });
+    $(function() {
+        $(".tentang").typed({
+            strings: ["TENTANG"],
+            typeSpeed: 0,
+            contentType: 'html',
+            showCursor: false,
+        });
+    });
+    $(function() {
+        $(".portofolio").typed({
+            strings: ["PORTOFOLIO"],
+            typeSpeed: 0,
+            contentType: 'html',
+            showCursor: false,
+        });
+    });
+    $(function() {
+        $(".layanan").typed({
+            strings: ["LAYANAN"],
+            typeSpeed: 0,
+            contentType: 'html',
+            showCursor: false,
+        });
+    });
