ea7a1c423f6b4ecf292cad83db5cfc6f07f5f863
aditya <adityamahaputra18@gmail.co>
2016-09-10 01:53:59 +0700

    AMD : update halaman depan

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,42 @@
-
-   // Mengatur Typed.js di #firstPage
-   $(function() {
-       $(".element").typed({
-           strings: ["Cepat", "Profesional", "Inovatif", "Kreatif"],
-           typeSpeed: 10,
-           contentType: 'html',
-           loop: true,
-           showCursor: false
-       });
-   });
-   $(function() {
-       $(".beranda").typed({
-           strings: ["BERANDA"],
-           typeSpeed: 0,
-           contentType: 'html',
-           showCursor: false,
-       });
-   });
-   $(function() {
-       $(".tentang").typed({
-           strings: ["TENTANG"],
-           typeSpeed: 0,
-           contentType: 'html',
-           showCursor: false,
-       });
-   });
-   $(function() {
-       $(".portofolio").typed({
-           strings: ["PORTOFOLIO"],
-           typeSpeed: 0,
-           contentType: 'html',
-           showCursor: false,
-       });
-   });
-   $(function() {
-       $(".layanan").typed({
-           strings: ["LAYANAN"],
-           typeSpeed: 0,
-           contentType: 'html',
-           showCursor: false,
-       });
-   }); 
+// Mengatur Typed.js di #firstPage
+$(function() {
+    $(".element").typed({
+        strings: ["Akurat", "Cepat", "Profesional", "Inovatif", "Kreatif"],
+        typeSpeed: 10,
+        contentType: 'html',
+        loop: true,
+        showCursor: false
+    });
+});
+$(function() {
+    $(".beranda").typed({
+        strings: ["BERANDA"],
+        typeSpeed: 0,
+        contentType: 'html',
+        showCursor: false,
+    });
+});
+$(function() {
+    $(".tentang").typed({
+        strings: ["TENTANG"],
+        typeSpeed: 0,
+        contentType: 'html',
+        showCursor: false,
+    });
+});
+$(function() {
+    $(".portofolio").typed({
+        strings: ["PORTOFOLIO"],
+        typeSpeed: 0,
+        contentType: 'html',
+        showCursor: false,
+    });
+});
+$(function() {
+    $(".layanan").typed({
+        strings: ["LAYANAN"],
+        typeSpeed: 0,
+        contentType: 'html',
+        showCursor: false,
+    });
+});
