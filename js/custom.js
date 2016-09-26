6a32dd63a11e314a811fc91d0fd2a187339e2f31
adit_mdd <adityamahaputra18@yahoo.com>
2016-09-26 16:54:44 +0700

    lebih keren

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,7 @@ $(function() {
         strings: ["Akurat", "Cepat", "Profesional", "Inovatif", "Kreatif"],
         typeSpeed: 10,
         contentType: 'html',
-        loop: true,
-        showCursor: false
+        loop: true
     });
 });
 $(function() {
@@ -34,7 +33,7 @@ $(function() {
 });
 $(function() {
     $(".layanan").typed({
-        strings: ["LAYANAN"],
+        strings: ["KONTAK KAMI"],
         typeSpeed: 0,
         contentType: 'html',
         showCursor: false,
